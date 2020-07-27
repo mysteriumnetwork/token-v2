23fc981265c94f102afae7629bd772d69ee55428
Jaro <jaro@nous.lt>
2020-07-28 00:41:08 +0300

    Delete unused vars

--- test/multisig.js
+++ test/multisig.js
@@ -6,15 +6,11 @@ chai.should()
 const expect = chai.expect
 
 const MystToken = artifacts.require("MystToken")
-const NextToken = artifacts.require("NextToken")
 const OriginalMystToken = artifacts.require("OriginalMystToken")
 const Multisig = artifacts.require("Multisig")
 
 const OneToken = web3.utils.toWei(new BN('100000000'), 'wei')  // In original contract MYST had 8 decimals
-const HalfToken = web3.utils.toWei(new BN('50000000'), 'wei')
-const OneEther = web3.utils.toWei(new BN(1), 'ether')
 const Multiplier = new BN('10000000000')                       // New token has 18 zeros instead of 8
-const Zero = new BN(0)
 const Empty = Buffer.from('')
 
 const states = {
@@ -27,7 +23,7 @@ const states = {
 }
 
 contract('Migration via mulisigs', ([txMaker, addressOne, addressTwo, addressThree, ...otherAddresses]) => {
-    let token, originalToken, tokenSupply, multisig
+    let token, originalToken, multisig
     before(async () => {
         multisig = await Multisig.new()
 
